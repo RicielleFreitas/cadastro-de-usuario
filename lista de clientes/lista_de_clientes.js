e91261a24bec4212e2173f4af64155fe994c1fc4
RicielleFreitas <riciellesiqueira881@gmail.com>
2022-04-01 02:29:46 -0300

    loop_objecto

--- lista de clientes/lista_de_clientes.js
+++ lista de clientes/lista_de_clientes.js
@@ -1,10 +1,25 @@
 //console.log('entrou')
-const listName = ['Making Love', 'Lucyus Clayton', 'João do Caminhão', 'Marcelino, Pão e Vinho', 'Boça dos Santos'
-]
-const listDom = document.getElementById('list-name');
-let setlistDom;
-listName.map((name) => {
-    setlistDom = setlistDom? setlistDom + `<li class="list-group-item">${name}</li>` : `<li class="list-group-item">${name}</li>`
-})
 
-listDom.innerHTML = setlistDom; 
+//const listName = ['Making Love', 'Lucyus Clayton', 'João do Caminhão', 'Marcelino, Pão e Vinho', 'Boça dos Santos'
+// LOOP array de stringa
+
+const clientes = [
+  {
+    nome: "Making Love",
+    idade: "21 anos",
+  },
+  {
+    nome: "Lucyus Clayton",
+    idade: "40 anos",
+  },
+];
+
+const listDom = document.getElementById("list-name");
+let setlistDom;
+clientes.map((semantico) => {
+  setlistDom = setlistDom
+    ? setlistDom +
+      `<li class="list-group-item">nome: ${semantico.nome}, idade: ${semantico.idade}</li>`
+      : `<li class="list-group-item">nome: ${semantico.nome}, idade: ${semantico.idade}</li>`;
+    });
+listDom.innerHTML = setlistDom;
